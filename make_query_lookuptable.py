44abdbfe58afe18848408e7cd71bd398b0fe5870
Saulo Soares <ocesaulo@gmail.com>
2017-02-15 18:37:30 -1000

    first commit of basic stuff

---
+++ make_query_lookuptable.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+# assembles a database of ARGO float profile data downloaded from GDAC
+# prepare look-up table array/list/dict
+# must contain EVERY PROFILE or EVERY FLOAT
+# each profile is a line in the dbase list
+# columns have the relevant profile info
+# the order of floats and profiles does not matter
+
+import numpy as np
+import glob
+import os
+import netCDF4
+import datetime
+from matplotlib.dates import num2date, datestr2num
+
+# usually 1st is good to pool all profile float files together as symlinks
+# this assumes the typical float file/dir structure after pull down from server
+
+source_dir = "/home/saulo/projects/data/ARGO_profiles/central_tsio/"
+dest_dir = "/home/saulo/projects/data/ARGO_profiles/central_tsio/profile_pool/"
+
+if not os.listdir(dest_dir):
+    os.system("cp -s " + source_dir + "*/*/*prof.nc " + dest_dir)
+else:
+    print "There is stuff in the destination directory, assuming its the data"
+
+# use glob to form a list of input files
+
+prof_files = glob.glob(dest_dir + '*.nc')
+# prof_files.sort(key=lambda x: [int(x.split('-')[2])])  # no need for sorting
+
+# prepare look-up table array/list/dict
+ 
+dbase = []  # dbase is the list of profiles that contains profile info
+
+# loop over input files, retrieve the necessary info and store it in the
+# appropriate place in
+for dafile in prof_files:
+    prof_data = netCDF4.Dataset(dafile)
+    nprofs = prof_data.dimensions["N_PROF"].size
+    nlevs = prof_data.dimensions["N_LEVELS"].size
+    try:
+        floatid = "".join(prof_data.variables["PLATFORM_NUMBER"][0,:].compressed())
+    except:
+        floatid = "".join(prof_data.variables["PLATFORM_NUMBER"][0,:])
+    ref_date = datetime.datetime.strptime("".join(prof_data.variables["REFERENCE_DATE_TIME"]), "%Y%m%d%H%M%S")
+    ref_date_num = datestr2num(datetime.datetime.strftime(ref_date, "%Y/%m/%d")) 
+    for n in range(0, nprofs):
+        juld = prof_data.variables["JULD"][n]
+        date_prof = num2date(ref_date_num + juld)  # will be UTC
+        year, mon, day = date_prof.year, date_prof.month, date_prof.day
+        lat = prof_data.variables["LATITUDE"][n]
+        lon = prof_data.variables["LONGITUDE"][n]
+        jul_qc = prof_data.variables["JULD_QC"][n]
+        pos_qc = prof_data.variables["POSITION_QC"][n]
+        dbase.append( (floatid, nprofs, nlevs, year, mon, day, juld, lat, lon, jul_qc, pos_qc, n) )
+dbase = np.array(dbase, dtype=[("floatid", '|S21'), ('nprofs', 'int32'),
+                               ('nlevs', 'int32'), ('year', 'int32'),
+                               ('month', 'int32'), ('day', 'int32'),
+                               ('juld', 'float32'), ('lat', 'float32'),
+                               ('lon', 'float32'), ('jul_qc', 'int32'),
+                               ('pos_qc', 'int32'), ('prof_n', 'int32')])
+np.savez(dest_dir + "argo_profile_info_database", dbase=dbase)
---
+++ manage_argo_profile_dbase.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+
+import numpy as np
+import netCDF4
+
+# read the database of argo profile data as set up
+dest_dir = "/home/saulo/projects/data/ARGO_profiles/central_tsio/profile_pool/"
+dbase = np.load(dest_dir + "argo_profile_info_database.npz")
+
+# come up with search criteria/what/who to query
+query = "year"
+crit = 2010
+
+# find the entries in database that match search
+match = dbase[dbase[query] == crit]
+
+# ideally file list would be only the unique, such that I only open the file
+# once and then take all the relevant profiles
+daids = list(set(match["floatid"]))  # has to be same as filestoread, could come before pathstofile
+
+# get paths to all the files:
+#filestoread = list(set(pathstofiles))  # could come before pathstofile 
+#pathstofiles = [dest_dir + m + "_prof.nc" for m in match["floatid"]]
+pathstofiles = [dest_dir + m + "_prof.nc" for m in daids]
+#prof_idx = match["prof_n"] # maybe deprecated
+
+# ideally file list would be only the unique, such that I only open the file
+# once and then take all the relevant profiles
+#filestoread = list(set(pathstofiles))  # could come before pathstofile 
+#daids = list(set(match["floatid"]))  # has to be same as filestoread, could come before pathstofile
+
+# read data in the matching lists
+#for n in range(0, len(prof_idx)):
+for n in range(0, len(filestoread)):
+    argo_data = netCDF4.Dataset(pathstofiles[n])
+    #  argo_data = netCDF4.Dataset(filestoread[n])
+    prof_idxs = match[match["floatid"]==daids[n]]["prof_n"]
+    # start pulling off data, should go all into some container class type (which would have limited functionality):
+    juld = argo_data.variables['JULD'][prof_idxs]  # at the moment contains multiple profiles
+    juld_qc = argo_data.variables['JULD_QC'][prof_idxs]
+    juld_loc = argo_data.variables['JULD_LOCATION'][prof_idxs]
+    lat = argo_data.variables['LATITUDE'][prof_idxs]
+    lon = argo_data.variables['LONGITUDE'][prof_idxs]
+    pos_qc = argo_data.variables['POSITION_QC'][prof_idxs]
+    prof_pres_qc = argo_data.variables['PROFILE_PRES_QC'][prof_idxs]
+    prof_sal_qc = argo_data.variables['PROFILE_PSAL_QC'][prof_idxs]
+    prof_temp_qc = argo_data.variables['PROFILE_TEMP_QC'][prof_idxs]
+    pres = argo_data.variables['PRES'][prof_idxs, :]
+    pres_qc = argo_data.variables['PRES_QC'][prof_idxs, :]
+    pres_a = argo_data.variables['PRES_ADJUSTED'][prof_idxs, :]
+    pres_a_qc = argo_data.variables['PRES_ADJUSTED_QC'][prof_idxs, :]
+    pres_a_er = argo_data.variables['PRES_ADJUSTED_ERROR'][prof_idxs, :]
+    sal = argo_data.variables['PSAL'][prof_idxs, :]
+    sal_qc = argo_data.variables['PSAL_QC'][prof_idxs, :]
+    sal_a = argo_data.variables['PSAL_ADJUSTED'][prof_idxs, :]
+    sal_a_qc = argo_data.variables['PSAL_ADJUSTED_QC'][prof_idxs, :]
+    sal_a_er = argo_data.variables['PSAL_ADJUSTED_ERROR'][prof_idxs, :]
+    temp = argo_data.variables['TEMP'][prof_idxs, :]
+    temp_qc = argo_data.variables['TEMP_QC'][prof_idxs, :]
+    temp_a = argo_data.variables['TEMP_ADJUSTED'][prof_idxs, :]
+    temp_a_qc = argo_data.variables['TEMP_ADJUSTED_QC'][prof_idxs, :]
+    temp_a_er = argo_data.variables['TEMP_ADJUSTED_ERROR'][prof_idxs, :]
+    argo_data.close()
+    # storing these data in arrays or lists? something that will allow easy further querying and sorting
+
